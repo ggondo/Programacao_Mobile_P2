82929cc3b0beffea850cfe9fdc734b676e299982
ggondo <gondo.g@aol.com>
2023-05-19 21:21:27 -0300

    chatgpt-rn

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,14 @@ const app = express()
 //middleware
 app.use(express.json())
 
+//const OPENAI_API_KEY = process.env.OPENAI_API_KEY
+
+const { OPENAI_API_KEY } = process.env
+
 app.get('/hello', (req, res) =>{
     res.json({mensagem: "Hello direto do Back end"})
 })
 
-const porta = 4000
+const porta = 4000 || 4000
 
 app.listen(4000, () => console.log (`Servidor ok. Porta ${porta}`))
