10c2b0d09475d8f059fa2e9dcc99d4e3143de0d1
ggondo <gondo.g@aol.com>
2023-05-27 01:18:30 -0300

    Versao 1.0 - FrontEnd - P2

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button , Input , Image } from 'react-native-elements';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <View style={styles.container}> 
+        <Image
+            source={require('./assets/figura1.jpg')}
+        />  
+        <View style={styles.topBar}>
+            <Text style={styles.title}>ChatGPT Responde:</Text>
+            <StatusBar style="auto" />
+        </View>
+
+        <View>
+          <Input 
+               placeholder="Digite aqui a sua frase para o ChatGPT!" 
+               style={styles.frase}
+          />
+        </View>
+        <Button 
+        title="Enviar"
+        style={styles.botao}
+        />
     </View>
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    backgroundColor: '#D8E5E9',
+    },
+  topBar: {
+    flexDirection: "row",
+    backgroundColor: "#328096",
+    height: 70,
+    justifyContent: "center"
+     },
+  title: {
+    color: "#FFFFFF",
+    fontSize: 25,
+    fontWeight: "bold",
+    margin: 15
   },
+  frase: {
+    marginTop: 60,
+    textAlign: "center"
+  },
+  botao: {
+    height: 100,
+    width: 300,
+    alignSelf: "center",
+    borderRadius: 5,
+    marginTop: 50,
+    fontsize: 50,
+    color: "#0000FF"
+
+  }
+  
 });
