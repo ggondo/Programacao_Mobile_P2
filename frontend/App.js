60d58dfaf9516685f7d1601fcc6674cd7f9462f8
ggondo <gondo.g@aol.com>
2023-06-01 11:58:45 -0300

    Versão 1.4 - Retirados comandos não utilizados no FrontEnd

--- frontend/App.js
+++ frontend/App.js
@@ -1,28 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React , { useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { Button , Input , ListItem } from 'react-native-elements';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button , Input } from 'react-native-elements';
 import axios from 'axios';
 
 export default function App() {
 
     const [texto, setTexto] = useState('')
-    const [textos, setTextos] = useState([])
-      
-        async function buscarFrase(){
+              
+       async function buscarFrase(){
       
         try{
 
           const response =  await axios
           .post('http://localhost:4000/sentimentos', {texto})
           setTexto(response.data.sentimento);
-           
+                    
           }catch(error){
-            
              console.log('ERRO', error);
            };
         }
-              
+          
     return (
      <View style={styles.container}> 
         <View style={styles.topBar}>
@@ -35,7 +33,7 @@ export default function App() {
                placeholder="Digite aqui a sua frase para o ChatGPT!" 
                style={styles.frase}
                onChangeText={(texto) => setTexto(texto)}
-               value={texto}
+               value={texto} 
           />
         </View>
         <Button 
@@ -43,23 +41,10 @@ export default function App() {
         style={styles.botao}
         onPress={buscarFrase}
         />
-        <View>
-          <FlatList
-            
-            data={textos}
-             renderItem={(texto) => (
-              <View style={styles.itemNaLista}>
-              <Text>{texto.item}</Text>
-             </View>
-            )}
-          />
-        </View>   
-        
+       
     </View>
-   
     );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -77,12 +62,12 @@ const styles = StyleSheet.create({
     fontSize: 25,
     fontWeight: "bold",
     margin: 15
-  },
+    },
   frase: {
     marginTop: 60,
     textAlign: "center",
     outlineStyle: 'none'
-  },
+    },
   botao: {
     height: 100,
     width: 300,
@@ -91,15 +76,5 @@ const styles = StyleSheet.create({
     marginTop: 50,
     fontsize: 50,
     color: "#0000FF"
-  },
-  itemNaLista: {
-    padding: 12,
-    borderColor: '#AAA',
-    borderWidth: 1,
-    backgroundColor: '#DDD',
-    marginBottom: 4,
-    textAlign: 'center',
-    borderRadius: 4  
-  }
-  
+    } 
 });
